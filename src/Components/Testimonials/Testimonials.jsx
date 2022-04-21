503a988e390b57ebabcf69b1dffcd34ce513d889
Yuvaraj-96 <yuvarajmechmailam@gmail.com>
2022-04-21 23:54:48 +0530

    added testimonials css code

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -3,6 +3,7 @@ import './Testimonials.css';
 import {Swiper,SwiperSlide} from 'swiper/react';
 import { Pagination } from "swiper";
 import "swiper/css/pagination";
+import 'swiper/css';
 import profilePic1 from "../../img/profile1.jpg";
 import profilePic2 from "../../img/profile2.jpg";
 import profilePic3 from "../../img/profile3.jpg";
@@ -40,6 +41,20 @@ const Testimonials = () => {
            <div className="blur t-blur1" style={{background:'var(--purple)'}}></div>
            <div className="blur t-blur2" style={{background:'skyblue'}}></div>
        </div>
+       <Swiper 
+       modules={[Pagination]}
+       slidesPerView={1} 
+       pagination={{clickable:true}}
+       >
+           {clients.map((client,index)=>{
+               return(<SwiperSlide key={index}>
+                   <div className="testimonal">
+                   <img src={client.img} alt=''/>
+                   <span>{client.review}</span>
+                   </div>
+                   </SwiperSlide>)
+           })}
+       </Swiper>
 
 
    </div>
